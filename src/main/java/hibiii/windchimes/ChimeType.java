4c107e93dae0e92c37301d2fcf5658dc7cd09bc3
Hibi <69405847+Hibiii@users.noreply.github.com>
2021-11-14 00:50:05 -0300

    Add copper chime

--- src/main/java/hibiii/windchimes/ChimeType.java
+++ src/main/java/hibiii/windchimes/ChimeType.java
@@ -20,6 +20,7 @@ public class ChimeType {
 
 	public static final ChimeType IRON;
 	public static final ChimeType BAMBOO;
+	public static final ChimeType COPPER;
 	public static final ChimeType INVALID;
 	
 	public static FabricBlockSettings settingsWith(Material in) {
@@ -31,28 +32,34 @@ public class ChimeType {
 			.noCollision();
 	}
 	
-	public static final Identifier IRON_LOUD_SOUND_ID, IRON_QUIET_SOUND_ID, BAMBOO_LOUD_SOUND_ID, BAMBOO_QUIET_SOUND_ID;
-	public static final SoundEvent IRON_LOUD_SOUND, IRON_QUIET_SOUND, BAMBOO_LOUD_SOUND, BAMBOO_QUIET_SOUND;
+	public static final Identifier IRON_LOUD_SOUND_ID, IRON_QUIET_SOUND_ID, BAMBOO_LOUD_SOUND_ID, BAMBOO_QUIET_SOUND_ID, COPPER_LOUD_SOUND_ID, COPPER_QUIET_SOUND_ID;
+	public static final SoundEvent IRON_LOUD_SOUND, IRON_QUIET_SOUND, BAMBOO_LOUD_SOUND, BAMBOO_QUIET_SOUND, COPPER_LOUD_SOUND, COPPER_QUIET_SOUND;
 	
 	static {
 		IRON_LOUD_SOUND_ID = new Identifier("windchimes:chime.iron.loud");
 		IRON_QUIET_SOUND_ID = new Identifier("windchimes:chime.iron.quiet");
 		BAMBOO_LOUD_SOUND_ID = new Identifier("windchimes:chime.bamboo.loud");
 		BAMBOO_QUIET_SOUND_ID = new Identifier("windchimes:chime.bamboo.quiet");
+		COPPER_LOUD_SOUND_ID = new Identifier("windchimes:chime.copper.loud");
+		COPPER_QUIET_SOUND_ID = new Identifier("windchimes:chime.copper.quiet");
 		
 		IRON_LOUD_SOUND = new SoundEvent(IRON_LOUD_SOUND_ID);
 		IRON_QUIET_SOUND = new SoundEvent(IRON_QUIET_SOUND_ID);
 		BAMBOO_LOUD_SOUND = new SoundEvent(BAMBOO_LOUD_SOUND_ID);
 		BAMBOO_QUIET_SOUND = new SoundEvent(BAMBOO_QUIET_SOUND_ID);
-	}
-	
-	static {
+		COPPER_LOUD_SOUND = new SoundEvent(COPPER_LOUD_SOUND_ID);
+		COPPER_QUIET_SOUND = new SoundEvent(COPPER_QUIET_SOUND_ID);
+
+
 		IRON = new ChimeType(
 			IRON_LOUD_SOUND, IRON_QUIET_SOUND,
 			new Identifier("windchimes:textures/iron_chime.png"));
 		BAMBOO = new ChimeType(
 			BAMBOO_LOUD_SOUND, BAMBOO_QUIET_SOUND,
 			new Identifier("windchimes:textures/bamboo_chime.png"));
+		COPPER = new ChimeType(
+			COPPER_LOUD_SOUND, COPPER_QUIET_SOUND,
+			new Identifier("windchimes:textures/copper_chime.png"));
 		INVALID = new ChimeType(
 			SoundEvents.ENTITY_PIG_AMBIENT,
 			SoundEvents.ENTITY_PIG_HURT,
--- src/main/java/hibiii/windchimes/Initializer.java
+++ src/main/java/hibiii/windchimes/Initializer.java
@@ -21,8 +21,9 @@ public class Initializer implements ModInitializer {
 	public static final WindchimeBlock BAMBOO_CHIME = new WindchimeBlock(
 			ChimeType.BAMBOO, ChimeType.settingsWith(Material.BAMBOO).sounds(BlockSoundGroup.BAMBOO));
 	
-	// TODO: Copper chimes
-	// TODO: Amethyst chimes
+	public static final Identifier COPPER_CHIME_ID = new Identifier("windchimes:copper_chime");
+	public static final WindchimeBlock COPPER_CHIME = new WindchimeBlock(
+			ChimeType.COPPER, ChimeType.settingsWith(Material.METAL).sounds(BlockSoundGroup.COPPER));
 	
 	public static BlockEntityType<WindchimeBlockEntity> CHIME_BLOCK_ENTITY;
 
@@ -40,7 +41,12 @@ public class Initializer implements ModInitializer {
 		Registry.register(Registry.ITEM, BAMBOO_CHIME_ID, new BlockItem(BAMBOO_CHIME, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
 		Registry.register(Registry.SOUND_EVENT, ChimeType.BAMBOO_LOUD_SOUND_ID, ChimeType.BAMBOO_LOUD_SOUND);
 		Registry.register(Registry.SOUND_EVENT, ChimeType.BAMBOO_QUIET_SOUND_ID, ChimeType.BAMBOO_QUIET_SOUND);
+
+		Registry.register(Registry.BLOCK, COPPER_CHIME_ID, COPPER_CHIME);
+		Registry.register(Registry.ITEM, COPPER_CHIME_ID, new BlockItem(COPPER_CHIME, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+		Registry.register(Registry.SOUND_EVENT, ChimeType.COPPER_LOUD_SOUND_ID, ChimeType.COPPER_LOUD_SOUND);
+		Registry.register(Registry.SOUND_EVENT, ChimeType.COPPER_QUIET_SOUND_ID, ChimeType.COPPER_QUIET_SOUND);
 		
-		CHIME_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, CHIME_BLOCK_ENTITY_ID, FabricBlockEntityTypeBuilder.create(WindchimeBlockEntity::new, IRON_CHIME, BAMBOO_CHIME).build(null));
+		CHIME_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, CHIME_BLOCK_ENTITY_ID, FabricBlockEntityTypeBuilder.create(WindchimeBlockEntity::new, IRON_CHIME, BAMBOO_CHIME, COPPER_CHIME).build(null));
 	}
 }
